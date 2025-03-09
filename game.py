bc2a1ec6bd77f6bc212143bb7da2a1d1a6c7fbb8
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-03-09 12:54:51 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -17,6 +17,9 @@ BORDER = pygame.Rect(445,0,10,HEIGHT)
 HEALTH_FONT = pygame.font.SysFont("Arial",45)
 WINNER_FONT = pygame.font.SysFont("Arial",95)
 
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
@@ -34,6 +37,48 @@ SPACE = pygame.transform.scale(pygame.image.load(os.path.join("assets","space_bg
 def draw_window(red,yellow,red_bullets,yellow_bullets,red_health,yellow_health):
     WINDOW.blit(SPACE,(0,0))
     pygame.draw.rect(WINDOW,BLACK,BORDER)
+    red_health_text = HEALTH_FONT.render("Health: "+str(red_health),1,WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: "+str(yellow_health),1,WHITE)
+    WINDOW.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10,10))
+    WINDOW.blit(yellow_health_text, (10,10))
+    WINDOW.blit(RED_SPACESHIP, (red.x,red.y))
+    WINDOW.blit(YELLOW_SPACESHIP, (yellow.x,yellow.y))
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WINDOW,RED, bullet)
+
+    for bullet in yellow_bullets:
+        pygame.draw.rect(WINDOW, YELLOW, bullet)
     pygame.display.update()
+
+def yellow_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:
+        yellow.y += VEL
+
+def red_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:
+        red.y += VEL
+
+def handle_bullets(red_bullets, yellow_bullets, yellow, red):
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)
+
 
 
